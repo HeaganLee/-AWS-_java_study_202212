4e5806915a38bd7c78173aa6000581f33186cbef
HeaganLee <ekzmfkdl12@gmail.com>
2023-01-19 17:37:36 +0900

    2023-01-19 데이터베이스 정규화

--- src/j20_json/builder/User.java
+++ src/j20_json/builder/User.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 // builder 안에는 allArgsConstructor를 포함한다.
-@Builder
+
 public class User {
 	
 	private String username;
@@ -14,38 +14,42 @@ public class User {
 	private String name;
 	private String email;
 	
-//	public static UserBuilder builder() {
-//		return new UserBuilder();
-//	}
-//	
-//	// static 클래스
-//	@Data
-//	public static class UserBuilder {
-//		private String username;
-//		private String password;
-//		private String name;
-//		private String email;
-//		
-//		public UserBuilder username(String username) {
-//			this.username = username;
-//			return this;
-//		}
-//		public UserBuilder password(String password) {
-//			this.password = password;
-//			return this;
-//		}
-//		public UserBuilder name(String name) {
-//			this.name = name;
-//			return this;
-//		}
-//		public UserBuilder email(String email) {
-//			this.email = email;
-//			return this;
-//		}
-//		
-//		public User build() {
-//			return new User(username, password, name, email);
-//		}
-//	}
+	public static UserBuilder builder() {
+		return new UserBuilder();
+	}
+	
+	public User(String username2, String password2, String name2, String email2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	// static 클래스
+	@Data
+	public static class UserBuilder {
+		private String username;
+		private String password;
+		private String name;
+		private String email;
+		
+		public UserBuilder username(String username) {
+			this.username = username;
+			return this;
+		}
+		public UserBuilder password(String password) {
+			this.password = password;
+			return this;
+		}
+		public UserBuilder name(String name) {
+			this.name = name;
+			return this;
+		}
+		public UserBuilder email(String email) {
+			this.email = email;
+			return this;
+		}
+		
+		public User build() {
+			return new User(username, password, name, email);
+		}
+	}
 
 }
